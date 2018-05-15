3e80f3b5a603131990ae2a1ec7c3cc5807231556
Stefan Schindler <dns2utf8@estada.ch>
2018-05-15 11:17:19 +0200

    v1.0.3 run_at document_end

--- hsp.js
+++ hsp.js
@@ -1,14 +1,22 @@
 /**
  * if not on single page, click the link if it exists
  */
-if (location.search.indexOf('seite=all') === -1) {
-    window.addEventListener("load", ev => {
-        let nodes = document.querySelectorAll("a[href]");
-        for (i in nodes) {
-            let a = nodes[i];
-            if (a.href.indexOf('seite=all') !== -1) {
-                a.click();
-            }
+(function() {
+
+let handler = ev => {
+    let nodes = document.querySelectorAll("a[href]");
+    for (i in nodes) {
+        let a = nodes[i];
+        if (a.href.indexOf('seite=all') !== -1) {
+            a.click();
         }
-    }, true);
+    }
+};
+    
+if (location.search.indexOf('seite=all') === -1) {
+    handler();
+    //window.addEventListener("load", handler, true);
+    //document.addEventListener("load", handler, true);
 }
+
+})();
